67ca1ac3958947ba1b7e0b7b5442cd18f6f7dda4
Rob C <robcabardo@gmail.com>
2015-06-23 15:17:52 -0500

    Add project files for CheckPassFaill app

---
+++ BasicFlowControl/CheckPassFail/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPassFail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // an integer value labelled 'mark' that is defined as zero
+            int mark = 0;
+
+            // begin if/else statement
+            if (mark >= 50)
+            {
+                // display a string to the console
+                Console.WriteLine("PASS");
+            }
+            else
+            {
+                Console.WriteLine("FAIL");
+            }
+
+            // rewritten using conditional operator (?:)
+            // Console.WriteLine(mark >= 50 ? "PASS" : "FAIL");
+        }
+    }
+}
