6fa4ff3175f64903c86b15cfa49b7102d37992a6
Rob C <robcabardo@gmail.com>
2015-06-25 01:21:56 -0500

    Update project files for CozaLozaWoza app

--- UsingLoops/CozaLozaWoza/Program.cs
+++ UsingLoops/CozaLozaWoza/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,78 @@ namespace CozaLozaWoza
 {
     class Program
     {
+        /// <summary>
+        /// Prints all numbers from 1 to 110 with 11 numbers per line.
+        /// Then replaces all multiples of 3, 5, and 7 with 'CozaLozaWoza',
+        /// all multiples of 3 and 5 with 'CozaLoza',
+        /// all multiples of 3 and 7 with 'CozaWoza',
+        /// all multiples of 5 and 7 with 'LozaWoza',
+        /// all multiples of just 3 with 'Coza',
+        /// all multiples of just 5 with 'Loza',
+        /// all multiples of just 7 with 'Woza',
+        /// </summary>
         static void Main(string[] args)
         {
+            var output = "";
+
+            var count = 1;
+
+            while (count < 111)
+            {
+                if (count % 3 == 0 && count % 5 == 0 && count % 7 == 0)
+                {
+                    output += "CozaLozaWoza ";
+                }
+
+                else if (count % 3 == 0 && count % 5 == 0)
+                {
+                    output += "CozaLoza ";
+                }
+                
+                else if (count % 5 == 0 && count % 7 == 0)
+                {
+                    output += "LozaWoza ";
+                }
+
+                else if (count % 3 == 0 && count % 7 == 0)
+                {
+                    output += "CozaWoza ";
+                }
+
+                else
+                {
+                    if (count % 3 == 0)
+                    {
+                        output += "Coza ";
+                    }
+                    
+                    else if (count % 5 == 0)
+                    {
+                        output += "Loza ";
+                    }
+
+                    else if (count % 7 == 0)
+                    {
+                        output += "Woza ";
+                    }
+
+                    else
+                    {
+                        output += count + " ";
+                    }
+                }
+
+                if (count % 11 == 0)
+                {
+                    output += "\n";
+                }
+
+                count++;
+            }
+
+            Console.Write(output);
+
+            Console.ReadKey();
         }
     }
 }
